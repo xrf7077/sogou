bd160975eb876cea841ba6c9d24ddf22e34a90f3
iron <1450269414@qq.com>
2019-11-14 14:52:55 +0800

    ygm完成

---
+++ src/components/Swiper.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="swiper-container ironman" >
+    <div class="swiper-wrapper">
+      <slot></slot>
+    </div>
+    <!-- 分页器 -->
+    <div class="swiper-pagination"></div>
+  </div>
+</template>
+<script>
+import Swiper from 'swiper'
+import 'swiper/css/swiper.min.css'
+export default {
+  mounted () {
+    /* eslint-disable no-new */
+    new Swiper('.ironman', this.options)
+  },
+  props: ['options', 'classname']
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -29,7 +29,7 @@ const routes = [{
   component: Home
 },
 {
-  path: '/Cate',
+  path: '/Cate/:newId',
   component: Cate
 },
 {
